fb62c72acc1052c089bfd27b7bea97d258caedaa
tseHaoSu <153491122+tseHaoSu@users.noreply.github.com>
2025-03-14 17:20:53 +1100

    fix the max UV and normal UV index

--- src/components/components/Card/Utils/UVIndex.tsx
+++ src/components/components/Card/Utils/UVIndex.tsx
@@ -10,31 +10,34 @@ const UVIndex = () => {
   const coordinates =
     currentLocation && locationCoordinates[currentLocation]
       ? locationCoordinates[currentLocation]
-      : { lat: -33.8688, lon: 151.2093 };
+      : { lat: -33.8688, lng: 151.2093 };
 
   const { data, isLoading, error } = useUVData({
     lat: coordinates.lat,
-    lng: coordinates.lon,
+    lng: coordinates.lng,
   });
 
   const setUVIndex = useInputQueryStore((state) => state.setUVIndex);
+  const setMaxUV = useInputQueryStore((state) => state.setMaxUV);
   const UVIndex = useInputQueryStore((state) => state.inputQuery.UVIndex);
 
   useEffect(() => {
     if (data) {
+      //set the global store of the UV index and Max UV in dex
       const roundedUVIndex = Math.round(data.result.uv);
+      const roundedMaxUVIndex = Math.round(data.result.uv_max);
       setUVIndex(roundedUVIndex);
+      setMaxUV(roundedMaxUVIndex);
     } else if (error) {
       setUVIndex(5);
     }
-  }, [data, setUVIndex, error]);
+  }, [data, setUVIndex, error, setMaxUV]);
 
   // Changed div to span for all return cases to avoid nesting issues
   if (isLoading) return <span>Loading UV data...</span>;
-  if (error) return <span>0</span>;
   if (!data) return null;
 
-  return <span>{UVIndex}</span>;
+  return <span>{UVIndex !== undefined ? UVIndex : 5}</span>;
 };
 
 export default UVIndex;
